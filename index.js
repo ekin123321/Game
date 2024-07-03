4c8242bbfeaf90f7881dc89ca305b89e388948c4
ekin123321 <120812084+ekin123321@users.noreply.github.com>
2024-07-03 15:08:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,48 @@ var artma = true;
 var bomb = false;
 var bombt = false
 var finishhimanimation = 0.1;
+function carpisma(rect1, rect2) {
+    return (
+        rect1.attackBox.position.x + rect1.attackBox.width >= rect2.pos.x &&
+        rect1.attackBox.position.x <= rect2.pos.x + rect2.width &&
+        rect1.attackBox.position.y + rect1.attackBox.height >= rect2.pos.y &&
+        rect1.attackBox.position.y <= rect2.pos.y + rect2.height
+    )
+}
+function carpisma2(rect1, rect2) {
+    return (
+        rect1.posx + rect1.width >= rect2.pos.x &&
+        rect1.posx <= rect2.pos.x + rect2.width &&
+        rect1.posy + rect1.height >= rect2.pos.y &&
+        rect1.posy <= rect2.pos.y + rect2.height
+    )
+}
+let timer = 60
+let timerid
+
+function say() {
+    if (timer > 0) {
+        timerid = setTimeout(say, 1000);
+        timer--
+        document.getElementById("timer").innerText = timer
+    }
+    if (timer === 0) {
+        kazananbelirle(player, enemy)
+    }
+
+}
+function kazananbelirle(player, enemy) {
+    clearTimeout(timerid)
+    document.getElementById("displayText").style.display = "flex"
+    if (player.health === enemy.health) {
+        document.getElementById("displayText").innerText = "BERABERE"
+    } else if (player.health > enemy.health) {
+        document.getElementById("displayText").innerText = "player kazandi"
+    } else if (player.health < enemy.health) {
+        document.getElementById("displayText").innerText = "enemy kazandi"
+    }
+
+}
 class Sprite {
     constructor({ pos, imageSrc, scale = 1, framesmax = 1, animOffset = { x: 0, y: 0 } }) {
         this.pos = pos
